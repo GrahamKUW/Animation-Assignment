8809521b97c7738b8dd25fcf2a9f805a14bba0a5
SuperRedSwan <Kavongraham2015@gmail.com>
2026-01-17 12:42:03 -0800

    Added UI
    Added Buttons that Play Different Animations and basic button and text system.

--- assignment.js
+++ assignment.js
@@ -6,7 +6,7 @@ class MSprite {
     cellSizeX = 16,
     cellSizeY = 16,
     filter = false,
-    isFlipped = false
+    isFlipped = false,
   ) {
     this.spritesheet = spritesheet;
     this.transform = transform;
@@ -18,7 +18,9 @@ class MSprite {
     this.flip = isFlipped;
     this.cellAmountX = this.spritesheet.width / this.cellSizeX; // treat as grid
     this.cellAmountY = this.spritesheet.height / this.cellSizeY; // treat as grid
-
+    this.isHidden = false;
+    this.width = this.cellSizeX * this.transform.scale.x;
+    this.height = this.cellSizeY * this.transform.scale.y;
     // center
     this.transform.center(cellSizeX, cellSizeY, isFlipped);
 
@@ -27,13 +29,21 @@ class MSprite {
         this.cellAmountX +
         " , " +
         this.cellAmountY +
-        " )"
+        " )",
     );
   }
 
   update() {}
 
   draw(ctx) {
+    if (this.isHidden) {
+      // if hidden don't draw
+      return;
+    }
+
+    this.width = this.cellSizeX * this.transform.scale.x;
+    this.height = this.cellSizeY * this.transform.scale.y;
+
     const cellImgX = this.cellPosX * this.cellSizeX;
     const cellImgY = this.cellPosY * this.cellSizeY;
     const flippedStatus = this.flip ? -1 : 1; // assume facing right from start
@@ -50,8 +60,8 @@ class MSprite {
       this.cellSizeY,
       flippedStatus * this.transform.position.x,
       this.transform.position.y,
-      this.cellSizeX * this.transform.scale.x,
-      this.cellSizeY * this.transform.scale.y
+      this.width,
+      this.height,
     );
 
     ctx.restore();
@@ -67,8 +77,16 @@ class SSprite extends MSprite {
       spritesheet.width,
       spritesheet.height,
       filter,
-      isFlipped
+      isFlipped,
     );
+  }
+
+  update() {
+    super.update();
+  }
+
+  draw() {
+    super.draw();
   }
 }
 
@@ -173,5 +191,101 @@ class Transform {
     this.position.x = x;
     this.position.y = y;
     this.center(width, height);
+  }
+}
+
+class Button {
+  constructor(buttonSprite, gameEngine, text = "Button", onPressButton = null) {
+    this.buttonSprite = buttonSprite;
+    this.rectTransform = {
+      width: buttonSprite.width,
+      height: buttonSprite.height,
+      x: buttonSprite.transform.position.x,
+      y: buttonSprite.transform.position.y,
+    };
+
+    this.gameEngine = gameEngine;
+    this.onPressButton = onPressButton;
+    this.text = text;
+
+    // Visual
+    this.originalScaleX = this.buttonSprite.transform.scale.x;
+    this.originalScaleY = this.buttonSprite.transform.scale.y;
+    this.isScaled = false;
+    this.scalefactor = 1.1;
+  }
+
+  update() {
+    //this.buttonSprite.update();
+    const mousePos = this.gameEngine.mouse; // mousePosition
+    const clickPos = this.gameEngine.click; // clcik position
+
+    const posInside =
+      mousePos === null ? false : this.pointInside(mousePos.x, mousePos.y);
+    const clickedInside =
+      clickPos === null ? false : this.pointInside(clickPos.x, clickPos.y);
+
+    // visual
+    if (posInside) {
+      this.buttonSprite.transform.scale.x =
+        this.originalScaleX * this.scalefactor;
+      this.buttonSprite.transform.scale.y =
+        this.originalScaleY * this.scalefactor;
+      this.isScaled = true;
+      //console.log("Button Hovered!");
+    } else {
+      this.buttonSprite.transform.scale.x = this.originalScaleX;
+      this.buttonSprite.transform.scale.y = this.originalScaleY;
+      this.isScaled = false;
+      //console.log("Button NOT Hovered!");
+    }
+
+    if (clickPos !== null && posInside && clickedInside) {
+      //console.log("Button Clicked");
+
+      if (this.onPressButton !== null) {
+        this.onPressButton();
+      }
+
+      this.gameEngine.click = null;
+
+      // visual
+      this.buttonSprite.transform.scale.x = this.originalScaleX;
+      this.buttonSprite.transform.scale.y = this.originalScaleY;
+    }
+  }
+
+  draw(ctx) {
+    this.buttonSprite.draw(ctx);
+    ctx.save();
+    let remSize = 3.2;
+    if (this.isScaled) {
+      remSize = remSize * this.scalefactor;
+    }
+    ctx.font = remSize + "rem Pixelify Sans";
+
+    // does not work properly
+    const xScaling = this.isScaled ? 1 : 1 * this.scalefactor;
+    const yScaling = this.isScaled ? 1 : 1 * this.scalefactor;
+
+    ctx.textAlign = "center";
+    ctx.textBaseline = "middle";
+    ctx.fillText(
+      this.text,
+      this.rectTransform.x + this.rectTransform.width / 2, // + offsetX,
+      this.rectTransform.y + this.rectTransform.height / 2, // + offsetY,
+    );
+
+    ctx.scale(xScaling, yScaling);
+    ctx.restore();
+  }
+
+  pointInside(px, py) {
+    return (
+      px >= this.rectTransform.x &&
+      px <= this.rectTransform.x + this.rectTransform.width &&
+      py >= this.rectTransform.y &&
+      py <= this.rectTransform.y + this.rectTransform.height
+    );
   }
 }
--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const gameEngine = new GameEngine();
 const ASSET_MANAGER = new AssetManager();
 
 ASSET_MANAGER.queueDownload("./assets/sprites/robosprite.png");
+ASSET_MANAGER.queueDownload("./assets/sprites/buttonEmpty.png");
 
 ASSET_MANAGER.downloadAll(() => {
   const canvas = document.getElementById("gameWorld");
@@ -16,29 +17,44 @@ ASSET_MANAGER.downloadAll(() => {
     false,
     true
   );*/
-  const msprite = new MSprite(
+  const animationMSprite = new MSprite(
     ASSET_MANAGER.getAsset("./assets/sprites/robosprite.png"),
     new Transform(ctx.canvas.width / 2, ctx.canvas.height / 2, 0, 10, 10),
     32,
     32,
     false,
-    false
+    false,
   );
 
+  const buttonSprites = [];
+  const buttonAmount = 5;
+  for (let i = 0; i < buttonAmount; i++) {
+    const buttonMSprite = new MSprite(
+      ASSET_MANAGER.getAsset("./assets/sprites/buttonEmpty.png"),
+      new Transform(80, 450 + 70 * i, 0, 5, 5),
+      28,
+      10,
+      false,
+      false,
+    );
+
+    buttonSprites.push(buttonMSprite);
+  }
+
   const animations = [
-    new Animation(msprite, 5, 0.5, 1, gameEngine),
-    new Animation(msprite, 6, 0.5, 2, gameEngine),
-    new Animation(msprite, 3, 0.5, 3, gameEngine),
-    new Animation(msprite, 2, 0.5, 4, gameEngine),
-    new Animation(msprite, 4, 0.5, 5, gameEngine),
-    new Animation(msprite, 2, 0.5, 6, gameEngine),
-    new Animation(msprite, 5, 0.5, 7, gameEngine),
-    new Animation(msprite, 5, 0.5, 8, gameEngine),
-    new Animation(msprite, 5, 0.5, 9, gameEngine),
-    new Animation(msprite, 11, 0.5, 10, gameEngine),
-    new Animation(msprite, 4, 0.5, 11, gameEngine),
-    new Animation(msprite, 10, 0.5, 12, gameEngine),
-    new Animation(msprite, 11, 0.5, 13, gameEngine),
+    new Animation(animationMSprite, 5, 0.5, 1, gameEngine),
+    new Animation(animationMSprite, 6, 0.5, 2, gameEngine),
+    new Animation(animationMSprite, 3, 0.5, 3, gameEngine),
+    new Animation(animationMSprite, 2, 0.5, 4, gameEngine),
+    new Animation(animationMSprite, 4, 0.5, 5, gameEngine),
+    new Animation(animationMSprite, 2, 0.5, 6, gameEngine),
+    new Animation(animationMSprite, 5, 0.5, 7, gameEngine),
+    new Animation(animationMSprite, 5, 0.5, 8, gameEngine),
+    new Animation(animationMSprite, 5, 0.5, 9, gameEngine),
+    new Animation(animationMSprite, 11, 0.5, 10, gameEngine),
+    new Animation(animationMSprite, 4, 0.5, 11, gameEngine),
+    new Animation(animationMSprite, 10, 0.5, 12, gameEngine),
+    new Animation(animationMSprite, 11, 0.5, 13, gameEngine),
   ];
 
   const animController = new AnimationController();
@@ -50,6 +66,37 @@ ASSET_MANAGER.downloadAll(() => {
   animController.switch(0);
 
   gameEngine.addEntity(animController);
+
+  const idleButton = new Button(buttonSprites[0], gameEngine, "Idle", () => {
+    animController.switch(0);
+  });
+
+  const walkButton = new Button(buttonSprites[1], gameEngine, "Walk", () => {
+    animController.switch(1);
+  });
+
+  const attackButton = new Button(
+    buttonSprites[2],
+    gameEngine,
+    "Attack",
+    () => {
+      animController.switch(6);
+    },
+  );
+
+  const hitButton = new Button(buttonSprites[3], gameEngine, "Item", () => {
+    animController.switch(11);
+  });
+
+  const deathButton = new Button(buttonSprites[4], gameEngine, "Death", () => {
+    animController.switch(12);
+  });
+
+  gameEngine.addEntity(idleButton);
+  gameEngine.addEntity(walkButton);
+  gameEngine.addEntity(attackButton);
+  gameEngine.addEntity(hitButton);
+  gameEngine.addEntity(deathButton);
 
   //gameEngine.addEntity(sprite);
   gameEngine.start();
